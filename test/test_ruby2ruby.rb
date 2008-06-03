d92485b427d9f6910865d469dab0673999b53994
Ryan Davis <ryand@zenspider.com>
2008-06-03 13:24:46 -0800

    Added more defensive programming in the tests to make it work with 1.9 and rubinius better.
    [git-p4: depot-paths = "//src/ruby2ruby/dev/": change = 3957]

--- test/test_ruby2ruby.rb
+++ test/test_ruby2ruby.rb
@@ -12,6 +12,8 @@ require 'pt_testcase'
 require 'fileutils'
 require 'tmpdir'
 
+FileUtils.rm_rf File.expand_path("~/.ruby_inline") # for self-translation
+
 class TestRuby2Ruby < Test::Unit::TestCase
   def setup
     super
@@ -22,7 +24,7 @@ class TestRuby2Ruby < Test::Unit::TestCase
     unless $DEBUG then
       FileUtils.rm_rf @rootdir
       ENV.delete 'INLINEDIR'
-    end if @rootdir
+    end if defined?(@rootdir) && @rootdir
   end
 
   def util_setup_inline
@@ -120,21 +122,17 @@ end
 
 def morph_and_eval(processor, target, gen, n)
   begin
-    old_name = target.name
-    new_name = target.name.sub(/\d*$/, gen.to_s)
+    processor = Object.const_get processor if Symbol === processor
+    target    = Object.const_get target    if Symbol === target
+    old_name  = target.name
+    new_name  = target.name.sub(/\d*$/, gen.to_s)
     ruby = processor.translate(target).sub(old_name, new_name)
 
     eval ruby
     target.constants.each do |constant|
       eval "#{new_name}::#{constant} = #{old_name}::#{constant}"
     end
-  rescue SyntaxError => e
-    warn "Self-Translation Generation #{n} failed:"
-    warn "#{e.class}: #{e.message}"
-    warn ""
-    warn ruby
-    warn ""
-  rescue => e
+  rescue Exception => e
     warn "Self-Translation Generation #{n} failed:"
     warn "#{e.class}: #{e.message}"
     warn ""
@@ -159,17 +157,17 @@ end
 # t  new    2    3
 
 # Self-Translation: 1st Generation - morph Ruby2Ruby using Ruby2Ruby
-morph_and_eval Ruby2Ruby, Ruby2Ruby, 2, 1 do
+morph_and_eval :Ruby2Ruby, :Ruby2Ruby, 2, 1 do
   class TestRuby2Ruby1 < TestRuby2Ruby
     def setup
       super
       @processor = Ruby2Ruby2.new
     end
   end
-end
+end unless SKIP_PROCS
 
 # Self-Translation: 2nd Generation - morph TestRuby2Ruby using Ruby2Ruby
-morph_and_eval Ruby2Ruby, TestRuby2Ruby, 2, 2 do
+morph_and_eval :Ruby2Ruby, :TestRuby2Ruby, 2, 2 do
   # Self-Translation: 3rd Generation - test Ruby2Ruby2 with TestRuby2Ruby1
   class TestRuby2Ruby3 < TestRuby2Ruby2
     def setup
@@ -177,14 +175,14 @@ morph_and_eval Ruby2Ruby, TestRuby2Ruby, 2, 2 do
       @processor = Ruby2Ruby2.new
     end
   end
-end
+end unless SKIP_PROCS
 
 # Self-Translation: 4th (and final) Generation - fully circular
-morph_and_eval(Ruby2Ruby2, Ruby2Ruby2, 3, 4) do
+morph_and_eval(:Ruby2Ruby2, :Ruby2Ruby2, 3, 4) do
   class TestRuby2Ruby4 < TestRuby2Ruby3
     def setup
       super
       @processor = Ruby2Ruby3.new
     end
   end
-end rescue nil # for Ruby2Ruby2 at the top
+end unless SKIP_PROCS
