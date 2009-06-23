439db5e68a31edb93aaa1a69325bdcad3335df83
Ryan Davis <ryand@zenspider.com>
2009-06-22 22:21:41 -0800

    + Overhauled 4-generation tests to use RubyParser. Much cleaner [git-p4: depot-paths = "//src/ruby2ruby/dev/": change = 5145]

--- test/test_ruby2ruby.rb
+++ test/test_ruby2ruby.rb
@@ -4,15 +4,11 @@ $TESTING = true
 
 $: << 'lib'
 
-SKIP_PROCS = ENV['FAST'] or RUBY_VERSION >= "1.9" or defined? RUBY_ENGINE
-
 require 'minitest/autorun'
 require 'ruby2ruby'
 require 'pt_testcase'
 require 'fileutils'
 require 'tmpdir'
-
-FileUtils.rm_rf File.expand_path("~/.ruby_inline") # for self-translation
 
 class R2RTestCase < ParseTreeTestCase
   def self.previous key
@@ -36,17 +32,12 @@ class R2RTestCase < ParseTreeTestCase
   end
 end
 
+start = __LINE__
+
 class TestRuby2Ruby < R2RTestCase
   def setup
     super
     @processor = Ruby2Ruby.new
-  end
-
-  def teardown
-    unless $DEBUG then
-      FileUtils.rm_rf @rootdir
-      ENV.delete 'INLINEDIR'
-    end if defined?(@rootdir) && @rootdir
   end
 
   def test_dregx_slash
@@ -122,9 +113,9 @@ class TestRuby2Ruby < R2RTestCase
                 s(:call,
                   s(:call, nil, :line, s(:arglist)),
                   :split,
-                  s(:arglist, s(:lit, /=/), s(:lit, 2))))))
+                  s(:arglist, s(:lit, /\=/), s(:lit, 2))))))
 
-    out = 'x(*line.split(/=/, 2))'
+    out = 'x(*line.split(/\=/, 2))'
     util_compare inn, out
   end
 
@@ -133,55 +124,11 @@ class TestRuby2Ruby < R2RTestCase
     assert_equal expected_eval, eval(expected_ruby) if expected_eval
   end
 
-  def util_setup_inline
-    @rootdir = File.join(Dir.tmpdir, "test_ruby_to_ruby.#{$$}")
-    Dir.mkdir @rootdir, 0700 unless test ?d, @rootdir
-    ENV['INLINEDIR'] = @rootdir
-  end
-
   def util_thingy(type)
     s(type,
       'blah"blah',
       s(:call, s(:lit, 1), :+, s(:arglist, s(:lit, 1))),
       s(:str, 'blah"blah/blah'))
-  end
-end
-
-##
-# Converts a +target+ using a +processor+ and converts +target+ name
-# in the source adding +gen+ to allow easy renaming.
-
-$broken = false
-def morph_and_eval(processor, target, gen, n)
-  return if $broken
-  begin
-    processor = Object.const_get processor if Symbol === processor
-    target    = Object.const_get target    if Symbol === target
-    old_name  = target.name
-    new_name  = target.name.sub(/\d*$/, gen.to_s)
-    ruby      = processor.translate(target).sub(old_name, new_name)
-
-    old, $-w = $-w, nil
-    eval ruby
-    $-w = old
-
-    target.constants.each do |constant|
-      eval "#{new_name}::#{constant} = #{old_name}::#{constant}"
-    end
-  rescue Exception => e
-    warn "Self-Translation Generation #{n} failed:"
-    warn "#{e.class}: #{e.message}"
-    warn e.backtrace.join("\n  ")
-    warn ""
-    warn ruby
-    warn ""
-    $broken = true
-  else
-    begin
-      yield if block_given?
-    rescue
-      # probably already handled
-    end
   end
 end
 
@@ -194,33 +141,44 @@ end
 # s
 # t  new    2    3
 
-# Self-Translation: 1st Generation - morph Ruby2Ruby using Ruby2Ruby
-morph_and_eval :Ruby2Ruby, :Ruby2Ruby, 2, 1 do
-  class TestRuby2Ruby1 < TestRuby2Ruby
-    def setup
-      super
-      @processor = Ruby2Ruby2.new
-    end
-  end
-end unless SKIP_PROCS
+tr2r = File.read(__FILE__).split(/\n/)[start+1..__LINE__-2].join("\n")
+ir2r = File.read("lib/ruby2ruby.rb")
 
-# Self-Translation: 2nd Generation - morph TestRuby2Ruby using Ruby2Ruby
-morph_and_eval :Ruby2Ruby, :TestRuby2Ruby, 2, 2 do
-  # Self-Translation: 3rd Generation - test Ruby2Ruby2 with TestRuby2Ruby1
-  class TestRuby2Ruby3 < TestRuby2Ruby2
-    def setup
-      super
-      @processor = Ruby2Ruby2.new
-    end
-  end
-end unless SKIP_PROCS
+require 'ruby_parser'
 
-# Self-Translation: 4th (and final) Generation - fully circular
-morph_and_eval(:Ruby2Ruby2, :Ruby2Ruby2, 3, 4) do
-  class TestRuby2Ruby4 < TestRuby2Ruby3
-    def setup
-      super
-      @processor = Ruby2Ruby3.new
-    end
+def silent_eval ruby
+  old, $-w = $-w, nil
+  eval ruby
+  $-w = old
+end
+
+def morph_and_eval src, from, to, processor
+  new_src = processor.new.process(RubyParser.new.process(src.sub(from, to)))
+  silent_eval new_src
+  new_src
+end
+
+____ = morph_and_eval tr2r, /TestRuby2Ruby/, 'TestRuby2Ruby2', Ruby2Ruby
+ruby = morph_and_eval ir2r, /Ruby2Ruby/,     'Ruby2Ruby2',     Ruby2Ruby
+____ = morph_and_eval ruby, /Ruby2Ruby2/,    'Ruby2Ruby3',     Ruby2Ruby2
+
+class TestRuby2Ruby1 < TestRuby2Ruby
+  def setup
+    super
+    @processor = Ruby2Ruby2.new
   end
-end unless SKIP_PROCS
+end
+
+class TestRuby2Ruby3 < TestRuby2Ruby2
+  def setup
+    super
+    @processor = Ruby2Ruby2.new
+  end
+end
+
+class TestRuby2Ruby4 < TestRuby2Ruby2
+  def setup
+    super
+    @processor = Ruby2Ruby3.new
+  end
+end
