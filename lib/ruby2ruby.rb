65ec59ddb1d1b137aeeaf5d880592c97620372cf
Ryan Davis <ryand@zenspider.com>
2013-02-07 15:31:15 -0800

    prepped for release [git-p4: depot-paths = "//src/ruby2ruby/dev/": change = 8184]

--- lib/ruby2ruby.rb
+++ lib/ruby2ruby.rb
@@ -24,7 +24,7 @@ class Regexp
 end
 
 class Ruby2Ruby < SexpProcessor
-  VERSION = '2.0.2'
+  VERSION = '2.0.3'
   LINE_LENGTH = 78
 
   BINARY = [:<=>, :==, :<, :>, :<=, :>=, :-, :+, :*, :/, :%, :<<, :>>, :**]
