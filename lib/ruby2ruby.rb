7bb4bcea797c8d7e613da526bc6096686c73485e
Ryan Davis <ryand@zenspider.com>
2013-04-25 15:06:15 -0800

    prepped for release [git-p4: depot-paths = "//src/ruby2ruby/dev/": change = 8458]

--- lib/ruby2ruby.rb
+++ lib/ruby2ruby.rb
@@ -29,7 +29,7 @@ end
 # Generate ruby code from a sexp.
 
 class Ruby2Ruby < SexpProcessor
-  VERSION = "2.0.4" # :nodoc:
+  VERSION = "2.0.5" # :nodoc:
 
   # cutoff for one-liners
   LINE_LENGTH = 78
